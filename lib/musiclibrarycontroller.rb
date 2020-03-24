d3e08e785c72837bb5cab1b5f7cd14af3e48e6b4
Ryna Truong <rynatruong@outlook.com>
2020-03-24 20:02:31 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -20,6 +20,21 @@ class MusicLibraryController
     if input != "exit"
       call
     end
+    
+    case input
+     when "list songs"
+       list_songs
+     when "list artists"
+       list_artists
+     when "list genres"
+       list_genres
+     when "list artist"
+       list_songs_by_artist
+     when "list genre"
+       list_songs_by_genre
+     when "play song"
+       play_song
+     end
   end
   
   def list_songs
