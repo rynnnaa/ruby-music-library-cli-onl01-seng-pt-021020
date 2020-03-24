89f2cebaadbca434475718a5e37cb9f668e6e25e
Ryna Truong <rynatruong@outlook.com>
2020-03-24 18:46:30 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -39,10 +39,8 @@ class MusicLibraryController
       call
     end
 
-  end
-
-  def list_songs
-    Song.all.sort_by(&:name).each.with_index(1) do |song_obj, idx|
-      puts "#{idx}. #{song_obj.artist.name} - #{song_obj.name} - #{song_obj.genre.name}"
-    end
+  # def list_songs
+  #   Song.all.sort_by(&:name).each.with_index(1) do |song_obj, idx|
+  #     puts "#{idx}. #{song_obj.artist.name} - #{song_obj.name} - #{song_obj.genre.name}"
+  #   end
   end
