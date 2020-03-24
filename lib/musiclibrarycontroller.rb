ff4bc103193744c91a509e8133642f0be1d5759a
Ryna Truong <rynatruong@outlook.com>
2020-03-24 19:34:53 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -15,7 +15,7 @@ class MusicLibraryController
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
     
-    input = gets.chomp
+    input = gets
     
     if input != "exit"
       call
@@ -53,5 +53,6 @@ class MusicLibraryController
   def list_songs_by_genre
     puts "Please enter the name of a genre:"
     input = gets
+    
   end
 end
